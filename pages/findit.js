99a3f053cdf5dc670c0103084592f9c329b52741
Jan Kirchner <42740468+Holsp@users.noreply.github.com>
2022-06-19 23:04:44 +0200

    vercel optimalization

--- pages/findit.js
+++ pages/findit.js
@@ -5,6 +5,6 @@ export default function FindIt() {
 
 
    return(
-      <img style={{width: "90vw", height: "80vh", margin: "5%"}}src="https://images2.imgbox.com/cb/86/j1odlUNu_o.png"></img>
+      <img alt="Don't let the image corner you" style={{width: "90vw", height: "80vh", margin: "5%"}}src="https://images2.imgbox.com/cb/86/j1odlUNu_o.png"></img>
    )
 }
